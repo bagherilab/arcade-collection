0092aba3a60253ceff16e4882af369420b583a0b
jessicasyu <jessicayu@u.northwestern.edu>
2022-10-19 17:10:51 -0400

    Fix merge dataframes to remove duplicate columns

--- src/arcade_collection/output/merge_parsed_results.py
+++ src/arcade_collection/output/merge_parsed_results.py
@@ -5,8 +5,8 @@ from prefect import task
 @task
 def merge_parsed_results(*results: pd.DataFrame) -> pd.DataFrame:
     for result in results:
-        result.set_index(["ID", "TICK"])
+        result.set_index(["ID", "TICK"], inplace=True)
 
     merged_results = pd.concat(results, axis=1)
 
-    return merged_results
+    return merged_results.reset_index()
