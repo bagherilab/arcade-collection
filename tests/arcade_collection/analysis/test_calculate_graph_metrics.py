776d4edd92ac306d15ff708aa13eb1d7d1e6fd28
Jacob Evarts <jacob.i.evarts@gmail.com>
2022-12-05 09:53:01 -0800

    mypy and linting

--- tests/arcade_collection/analysis/test_calculate_graph_metrics.py
+++ tests/arcade_collection/analysis/test_calculate_graph_metrics.py
@@ -7,7 +7,7 @@ from arcade_collection.analysis import calculate_graph_metrics
 
 
 class TestCalculateGraphMetrics(unittest.TestCase):
-    def test_calculate_graph_metrics_given_unweighted_edges_calculates_metrics(self):
+    def test_calculate_graph_metrics_given_unweighted_edges_calculates_metrics(self) -> None:
         edges = [[1, 3], [2, 3], [3, 4], [4, 5], [5, 6], [5, 7], [6, 7]]
         expected_metrics = {
             "nodes": 7,
@@ -31,9 +31,9 @@ class TestCalculateGraphMetrics(unittest.TestCase):
             with self.subTest(key=key):
                 self.assertAlmostEqual(expected_metrics[key], returned_metrics[key], places=5)
 
-    def test_calculate_graph_metrics_given_weighted_edges_calculates_metrics(self):
+    def test_calculate_graph_metrics_given_weighted_edges_calculates_metrics(self) -> None:
         edges = [[1, 3], [2, 3], [3, 4], [4, 5], [5, 6], [5, 7], [6, 7]]
-        weights = [2, 1, 3, 3, 1, 5, 1]
+        weights = [2.0, 1.0, 3.0, 3.0, 1.0, 5.0, 1.0]
         expected_metrics = {
             "nodes": 7,
             "edges": 7,
@@ -56,7 +56,7 @@ class TestCalculateGraphMetrics(unittest.TestCase):
             with self.subTest(key=key):
                 self.assertAlmostEqual(expected_metrics[key], returned_metrics[key], places=5)
 
-    def test_calculate_graph_metrics_given_disconnected_eges_calculates_metrics(self):
+    def test_calculate_graph_metrics_given_disconnected_eges_calculates_metrics(self) -> None:
         edges = [[1, 3], [2, 3], [3, 4], [5, 6], [5, 7], [6, 7]]
         expected_metrics = {
             "nodes": 7,
@@ -80,8 +80,8 @@ class TestCalculateGraphMetrics(unittest.TestCase):
             with self.subTest(key=key):
                 self.assertAlmostEqual(expected_metrics[key], returned_metrics[key], places=5)
 
-    def test_calculate_graph_metrics_given_empty_edges_returns_none(self):
-        edges = []
+    def test_calculate_graph_metrics_given_empty_edges_returns_none(self) -> None:
+        edges : list[list[int]] = []
         with self.assertRaises(ValueError):
             calculate_graph_metrics.fn(edges)
 
