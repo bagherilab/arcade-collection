2c9affec0eafa9d3dcc324fb6e6790ad0265f2e5
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-03-29 18:15:32 -0400

    Add task decorator to init imports

--- src/arcade_collection/input/__init__.py
+++ src/arcade_collection/input/__init__.py
@@ -1,6 +1,3 @@
-import importlib
-import sys
-
 from prefect import task
 
 from .convert_to_cells_file import convert_to_cells_file
@@ -9,15 +6,8 @@ from .generate_setup_file import generate_setup_file
 from .group_template_conditions import group_template_conditions
 from .merge_region_samples import merge_region_samples
 
-TASK_MODULES = [
-    convert_to_cells_file,
-    convert_to_locations_file,
-    generate_setup_file,
-    group_template_conditions,
-    merge_region_samples,
-]
-
-for task_module in TASK_MODULES:
-    MODULE_NAME = task_module.__name__
-    module = importlib.import_module(f".{MODULE_NAME}", package=__name__)
-    setattr(sys.modules[__name__], MODULE_NAME, task(getattr(module, MODULE_NAME)))
+convert_to_cells_file = task(convert_to_cells_file)
+convert_to_locations_file = task(convert_to_locations_file)
+generate_setup_file = task(generate_setup_file)
+group_template_conditions = task(group_template_conditions)
+merge_region_samples = task(merge_region_samples)
