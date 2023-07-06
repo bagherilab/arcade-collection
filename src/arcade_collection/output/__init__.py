1196a8738dc9e871d8d7ab6c74926a303fe127dd
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-07-06 11:33:24 -0400

    Add task for converting to colorizer format

--- src/arcade_collection/output/__init__.py
+++ src/arcade_collection/output/__init__.py
@@ -1,6 +1,7 @@
 from prefect import task
 
 from .convert_model_units import convert_model_units
+from .convert_to_colorizer import convert_to_colorizer
 from .convert_to_images import convert_to_images
 from .convert_to_meshes import convert_to_meshes
 from .convert_to_simularium import convert_to_simularium
@@ -11,6 +12,7 @@ from .parse_cells_file import parse_cells_file
 from .parse_locations_file import parse_locations_file
 
 convert_model_units = task(convert_model_units)
+convert_to_colorizer = task(convert_to_colorizer)
 convert_to_images = task(convert_to_images)
 convert_to_meshes = task(convert_to_meshes)
 convert_to_simularium = task(convert_to_simularium)
