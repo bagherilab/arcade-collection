b5cd4bd1af3c1bf2d247480ccace0ebc815b1488
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-08-23 20:09:16 -0400

    Add center and invert options to convert to meshes

--- src/arcade_collection/output/convert_to_meshes.py
+++ src/arcade_collection/output/convert_to_meshes.py
@@ -8,10 +8,17 @@ from arcade_collection.output.get_location_voxels import get_location_voxels
 
 
 def convert_to_meshes(
-    series_key: str, data_tar: tarfile.TarFile, frame_spec: tuple[int, int, int], regions: list[str]
+    series_key: str,
+    data_tar: tarfile.TarFile,
+    frame_spec: tuple[int, int, int],
+    regions: list[str],
+    box: tuple[int, int, int],
+    invert: bool = False,
 ) -> list[tuple[int, int, str, str]]:
     frames = list(np.arange(*frame_spec))
     meshes = []
+
+    length, width, height = box
 
     for frame in frames:
         locations = extract_tick_json(data_tar, series_key, frame, "LOCATIONS")
@@ -21,7 +28,7 @@ def convert_to_meshes(
 
             for region in regions:
                 voxels = [
-                    (z, y, x)
+                    (x, width - y, z)
                     for x, y, z in get_location_voxels(
                         location, region if region != "DEFAULT" else None
                     )
@@ -30,58 +37,60 @@ def convert_to_meshes(
                 if len(voxels) == 0:
                     continue
 
-                array = make_mesh_array(voxels)
-                mesh = make_mesh_object(array)
+                center = list(np.array(voxels).mean(axis=0))
+                array = make_mesh_array(voxels, length, width, height)
+                mesh = make_mesh_object(array, center, invert)
 
                 meshes.append((frame, location_id, region, mesh))
 
     return meshes
 
 
-def make_mesh_array(voxels: list[tuple[int, int, int]]) -> np.ndarray:
-    mins = np.min(voxels, axis=0)
-    maxs = np.max(voxels, axis=0)
-    height, width, length = np.subtract(maxs, mins) + 3
-    array = np.zeros((height, width, length), dtype=np.uint8)
-
-    voxels_transposed = [voxel - mins + 1 for voxel in voxels]
-    array[tuple(np.transpose(voxels_transposed))] = 7
+def make_mesh_array(
+    voxels: list[tuple[int, int, int]], length: int, width: int, height: int
+) -> np.ndarray:
+    # Create array.
+    array = np.zeros((length, width, height), dtype=np.uint8)
+    array[tuple(np.transpose(voxels))] = 7
 
     # Get set of zero neighbors for all voxels.
     offsets = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
     neighbors = {
-        (z + i, y + j, x + k)
-        for z, y, x in voxels_transposed
-        for k, j, i in offsets
-        if array[z + k, y + j, x + i] == 0
+        (x + i, y + j, z + k)
+        for x, y, z in voxels
+        for i, j, k in offsets
+        if array[x + i, y + j, z + k] == 0
     }
 
     # Remove invalid neighbors on borders.
     neighbors = {
-        (z, y, x)
-        for z, y, x in neighbors
+        (x, y, z)
+        for x, y, z in neighbors
         if x != 0 and y != 0 and z != 0 and x != length - 1 and y != width - 1 and z != height - 1
     }
 
     # Smooth array levels based on neighbor counts.
-    for z, y, x in neighbors:
-        array[z, y, x] = sum(array[z + k, y + j, x + i] == 7 for k, j, i in offsets) + 1
+    for x, y, z in neighbors:
+        array[x, y, z] = sum(array[x + i, y + j, z + k] == 7 for i, j, k in offsets) + 1
 
     return array
 
 
-def make_mesh_object(array: np.ndarray) -> str:
+def make_mesh_object(array: np.ndarray, center: list[float], invert: bool = False) -> str:
     verts, faces, normals, _ = measure.marching_cubes(array, level=3, allow_degenerate=False)
     mesh = ""
     faces = faces + 1
 
     for item in verts:
-        mesh += f"v {item[0]} {item[1]} {item[2]}\n"
+        mesh += f"v {item[0] - center[0]} {item[1] - center[1]} {item[2] - center[2]}\n"
 
     for item in normals:
         mesh += f"vn {item[0]} {item[1]} {item[2]}\n"
 
     for item in faces:
-        mesh += f"f {item[2]}//{item[2]} {item[1]}//{item[1]} {item[0]}//{item[0]}\n"
+        if invert:
+            mesh += f"f {item[0]}//{item[0]} {item[1]}//{item[1]} {item[2]}//{item[2]}\n"
+        else:
+            mesh += f"f {item[2]}//{item[2]} {item[1]}//{item[1]} {item[0]}//{item[0]}\n"
 
     return mesh
