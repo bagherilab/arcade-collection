17f81dc17ef478c91c77a193f504b4197adc1a3a
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-09-06 12:09:01 -0400

    Add convert to projection task

--- src/arcade_collection/output/convert_to_projection.py
+++ src/arcade_collection/output/convert_to_projection.py
@@ -1,87 +1,79 @@
 import tarfile
 from typing import Optional
 
-import matplotlib.figure as mpl
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
-from arcade_collection.output import extract_tick_json, get_location_voxels
-from prefect import task
+from matplotlib.patches import Rectangle
 
-from cell_abm_pipeline.utilities.plot import (
-    add_frame_scalebar,
-    add_frame_timestamp,
-    make_grid_figure,
-)
+from arcade_collection.output.extract_tick_json import extract_tick_json
+from arcade_collection.output.get_location_voxels import get_location_voxels
 
 
-@task
-def make_projection_frame(
-    keys: list[tuple[str, int]],
-    locations: dict[tuple[str, int], tarfile.TarFile],
-    name: str,
-    tick: int,
-    scale: int,
+def convert_to_projection(
+    series_key: str,
+    data_tar: tarfile.TarFile,
+    frame: int,
+    regions: list[str],
+    box: tuple[int, int, int],
     ds: float,
     dt: float,
-    box: tuple[int, int, int],
-    regions: list[str],
-) -> mpl.Figure:
-    fig, gridspec, indices = make_grid_figure(keys)
+    scale: int,
+) -> mpl.figure.Figure:
+    fig = plt.figure(figsize=(4, 4), constrained_layout=True)
     length, width, height = box
 
-    for i, j, (key, seed) in indices:
-        ax = fig.add_subplot(gridspec[i, j])
-        ax.set_title(f"{key} [{seed}]")
-        ax.invert_yaxis()
-        ax.get_xaxis().set_ticks([])
-        ax.get_yaxis().set_ticks([])
-        ax.set_xlim([0, length - 1])
-        ax.set_ylim([width - 1, 0])
-        ax.set(aspect=1)
+    ax = fig.add_subplot()
 
-        ax_horz = ax.inset_axes([0, 1.005, 1, height / width], sharex=ax)
-        ax_horz.set_ylim([0, height - 1])
-        ax_horz.get_yaxis().set_ticks([])
+    ax.invert_yaxis()
+    ax.get_xaxis().set_ticks([])
+    ax.get_yaxis().set_ticks([])
+    ax.set_xlim([0, length - 1])
+    ax.set_ylim([width - 1, 0])
+    ax.set_box_aspect(1)
 
-        ax_vert = ax.inset_axes([1.005, 0, height / length, 1], sharey=ax)
-        ax_vert.set_xlim([0, height - 1])
-        ax_vert.get_xaxis().set_ticks([])
+    ax_horz = ax.inset_axes([0, 1.005, 1, height / width], sharex=ax)
+    ax_horz.set_ylim([0, height - 1])
+    ax_horz.get_yaxis().set_ticks([])
 
-        tick_json = extract_tick_json.fn(
-            locations[(key, seed)], f"{name}_{key}_{seed:04d}", tick, "LOCATIONS"
-        )
+    ax_vert = ax.inset_axes([1.005, 0, height / length, 1], sharey=ax)
+    ax_vert.set_xlim([0, height - 1])
+    ax_vert.get_xaxis().set_ticks([])
 
-        if len(regions) == 1:
-            region = None if regions[0] == "DEFAULT" else regions[0]
+    locations = extract_tick_json(data_tar, series_key, frame, "LOCATIONS")
 
-            arr_1 = create_projection_array(tick_json, length, width, height, region)
-            ax.imshow(arr_1, cmap="bone", interpolation="none", vmin=0, vmax=1)
+    if len(regions) == 1:
+        region = None if regions[0] == "DEFAULT" else regions[0]
 
-            arr_2 = create_projection_array(tick_json, length, width, height, region, (0, 2, 1))
-            ax_horz.imshow(arr_2, cmap="bone", interpolation="none", vmin=0, vmax=1)
+        arr_1 = create_projection_array(locations, length, width, height, region)
+        ax.imshow(arr_1, cmap="bone", interpolation="none", vmin=0, vmax=1)
 
-            arr_3 = create_projection_array(tick_json, length, width, height, region, (2, 1, 0))
-            ax_vert.imshow(arr_3, cmap="bone", interpolation="none", vmin=0, vmax=1)
-        elif len(regions) == 2:
-            region_a = None if regions[0] == "DEFAULT" else regions[0]
-            region_b = None if regions[1] == "DEFAULT" else regions[1]
+        arr_2 = create_projection_array(locations, length, width, height, region, (0, 2, 1))
+        ax_horz.imshow(arr_2, cmap="bone", interpolation="none", vmin=0, vmax=1)
 
-            arr_a1 = create_projection_array(tick_json, length, width, height, region_a)
-            arr_b1 = create_projection_array(tick_json, length, width, height, region_b)
-            arr_1 = join_projection_arrays(arr_a1, arr_b1)
-            ax.imshow(arr_1, interpolation="none")
+        arr_3 = create_projection_array(locations, length, width, height, region, (2, 1, 0))
+        ax_vert.imshow(arr_3, cmap="bone", interpolation="none", vmin=0, vmax=1)
+    elif len(regions) == 2:
+        region_a = None if regions[0] == "DEFAULT" else regions[0]
+        region_b = None if regions[1] == "DEFAULT" else regions[1]
 
-            arr_a2 = create_projection_array(tick_json, length, width, height, region_a, (0, 2, 1))
-            arr_b2 = create_projection_array(tick_json, length, width, height, region_b, (0, 2, 1))
-            arr_2 = join_projection_arrays(arr_a2, arr_b2)
-            ax_horz.imshow(arr_2, interpolation="none")
+        arr_a1 = create_projection_array(locations, length, width, height, region_a)
+        arr_b1 = create_projection_array(locations, length, width, height, region_b)
+        arr_1 = join_projection_arrays(arr_a1, arr_b1)
+        ax.imshow(arr_1, interpolation="none")
 
-            arr_a3 = create_projection_array(tick_json, length, width, height, region_a, (2, 1, 0))
-            arr_b3 = create_projection_array(tick_json, length, width, height, region_b, (2, 1, 0))
-            arr_3 = join_projection_arrays(arr_a3, arr_b3)
-            ax_vert.imshow(arr_3, interpolation="none")
+        arr_a2 = create_projection_array(locations, length, width, height, region_a, (0, 2, 1))
+        arr_b2 = create_projection_array(locations, length, width, height, region_b, (0, 2, 1))
+        arr_2 = join_projection_arrays(arr_a2, arr_b2)
+        ax_horz.imshow(arr_2, interpolation="none")
 
-        add_frame_timestamp(ax, length, width, dt, tick, "#ffffff")
-        add_frame_scalebar(ax, length, width, ds, scale, "#ffffff")
+        arr_a3 = create_projection_array(locations, length, width, height, region_a, (2, 1, 0))
+        arr_b3 = create_projection_array(locations, length, width, height, region_b, (2, 1, 0))
+        arr_3 = join_projection_arrays(arr_a3, arr_b3)
+        ax_vert.imshow(arr_3, interpolation="none")
+
+    add_frame_timestamp(ax, length, width, dt, frame, "#ffffff")
+    add_frame_scalebar(ax, length, width, ds, scale, "#ffffff")
 
     return fig
 
@@ -97,7 +89,7 @@ def create_projection_array(
     array = np.zeros((length, width, height))
 
     for location in locations:
-        voxels = get_location_voxels.fn(location, region)
+        voxels = get_location_voxels(location, region)
 
         if len(voxels) == 0:
             continue
@@ -141,3 +133,45 @@ def join_projection_arrays(array_a: np.ndarray, array_b: np.ndarray) -> np.ndarr
     array[:, :, 2] = np.maximum(array[:, :, 2], array_b)
 
     return array
+
+
+def add_frame_timestamp(
+    ax: mpl.axes.Axes, length: int, width: int, dt: float, frame: int, color: str
+) -> None:
+    hours, minutes = divmod(round(frame * dt, 2), 1)
+    timestamp = f"{int(hours):02d}H:{round(minutes*60):02d}M"
+
+    ax.text(
+        0.03 * length,
+        0.96 * width,
+        timestamp,
+        fontfamily="monospace",
+        fontsize=20,
+        color=color,
+        fontweight="bold",
+    )
+
+
+def add_frame_scalebar(
+    ax: mpl.axes.Axes, length: int, width: int, ds: float, scale: int, color: str
+) -> None:
+    scalebar = scale / ds
+
+    ax.add_patch(
+        Rectangle(
+            (0.95 * length - scalebar, 0.92 * width),
+            scalebar,
+            0.01 * width,
+            snap=True,
+            color=color,
+        )
+    )
+
+    ax.text(
+        0.95 * length - scalebar / 2,
+        0.975 * width,
+        f"{scale} $\\mu$m",
+        fontsize=10,
+        color=color,
+        horizontalalignment="center",
+    )
