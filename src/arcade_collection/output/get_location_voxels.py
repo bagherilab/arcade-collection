7364fc2fdd4aa096469a9b9a2af40cf7ecf94149
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2023-09-06 14:28:07 -0400

    Fix get location voxels return type (#60)

--- src/arcade_collection/output/get_location_voxels.py
+++ src/arcade_collection/output/get_location_voxels.py
@@ -3,9 +3,9 @@ from typing import Optional
 
 def get_location_voxels(location: dict, region: Optional[str] = None) -> list[tuple[int, int, int]]:
     voxels = [
-        voxel
+        (x, y, z)
         for loc in location["location"]
-        for voxel in loc["voxels"]
+        for x, y, z in loc["voxels"]
         if not region or loc["region"] == region
     ]
     return voxels
