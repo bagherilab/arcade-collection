04eaa4d559ab958afd43f5a94b02694961d51acd
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2023-09-15 12:01:38 -0400

    Move convert tasks into convert package (#62)
    * Move convert tasks to dedicated package
    
    * Update inits for convert package
    
    * Fix init docstring

--- src/arcade_collection/output/__init__.py
+++ src/arcade_collection/output/__init__.py
@@ -3,11 +3,6 @@
 from prefect import task
 
 from .convert_model_units import convert_model_units
-from .convert_to_colorizer import convert_to_colorizer
-from .convert_to_images import convert_to_images
-from .convert_to_meshes import convert_to_meshes
-from .convert_to_projection import convert_to_projection
-from .convert_to_simularium import convert_to_simularium
 from .extract_tick_json import extract_tick_json
 from .get_location_voxels import get_location_voxels
 from .merge_parsed_results import merge_parsed_results
@@ -15,11 +10,6 @@ from .parse_cells_file import parse_cells_file
 from .parse_locations_file import parse_locations_file
 
 convert_model_units = task(convert_model_units)
-convert_to_colorizer = task(convert_to_colorizer)
-convert_to_images = task(convert_to_images)
-convert_to_meshes = task(convert_to_meshes)
-convert_to_projection = task(convert_to_projection)
-convert_to_simularium = task(convert_to_simularium)
 extract_tick_json = task(extract_tick_json)
 get_location_voxels = task(get_location_voxels)
 merge_parsed_results = task(merge_parsed_results)
