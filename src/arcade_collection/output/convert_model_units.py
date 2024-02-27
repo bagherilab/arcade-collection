78de359910c4f5bf150563a430fee283605055b0
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-02-27 11:28:21 -0500

    Add docstrings to convert model units task

--- src/arcade_collection/output/convert_model_units.py
+++ src/arcade_collection/output/convert_model_units.py
@@ -10,6 +10,48 @@ def convert_model_units(
     dt: Optional[float],
     regions: Optional[Union[list[str], str]] = None,
 ) -> None:
+    """
+    Converts data from simulation units to true units.
+
+    Simulations use spatial unit of voxels and temporal unit of ticks. Spatial
+    resolution (microns/voxel) and temporal resolution (hours/tick) are used to
+    convert data to true units. If spatial or temporal resolution is not given,
+    they will be estimated from the ``KEY`` column of the data.
+
+    The following columns are added to the data:
+
+    =============  ===================  =============================
+    Target column  Source column(s)      Calculation
+    =============  ===================  =============================
+    ``time``       ``TICK``             ``dt * TICK``
+    ``volume``     ``NUM_VOXELS``       ``ds * ds * ds * NUM_VOXELS``
+    ``height``     ``MAX_Z`` ``MIN_Z``  ``ds * (MAX_Z - MIN_Z + 1)``
+    ``cx``         ``CX``               ``ds * CX``
+    ``cy``         ``CY``               ``ds * CY``
+    ``cz``         ``CZ``               ``ds * CZ``
+    =============  ===================  =============================
+
+    For each region (other than ``DEFAULT``), volume and height are calculated:
+
+    =================  =================================  ==========================================
+    Target column      Source column(s)                   Calculation
+    =================  =================================  ==========================================
+    ``volume.REGION``  ``NUM_VOXELS.REGION``              ``ds * ds * ds * NUM_VOXELS.REGION``
+    ``height.REGION``  ``MAX_Z.REGION`` ``MIN_Z.REGION``  ``ds * (MAX_Z.REGION - MIN_Z.REGION + 1)``
+    =================  =================================  ==========================================
+
+    Parameters
+    ----------
+    data
+        Parsed simulation data.
+    ds
+        Spatial resolution in microns/voxel, use None to estimate from keys.
+    dt
+        Temporal resolution in hours/tick, use None to estimate from keys.
+    regions
+        List of regions.
+    """
+
     if dt is None:
         dt = data["KEY"].apply(estimate_temporal_resolution)
 
@@ -48,6 +90,24 @@ def convert_model_units(
 
 
 def estimate_temporal_resolution(key: str) -> float:
+    """
+    Estimates temporal resolution based on condition key.
+
+    If the key contains ``DT##``, where ``##`` denotes the temporal resolution
+    in minutes/tick, temporal resolution is estimated from ``##``. Otherwise,
+    the default temporal resolution is 1 hours/tick.
+
+    Parameters
+    ----------
+    key
+        Condition key.
+
+    Returns
+    -------
+    :
+        Temporal resolution (hours/tick).
+    """
+
     match = re.findall(r"[_]?DT([0-9]+)[_]?", key)
 
     if len(match) == 1:
@@ -57,6 +117,24 @@ def estimate_temporal_resolution(key: str) -> float:
 
 
 def estimate_spatial_resolution(key: str) -> float:
+    """
+    Estimates spatial resolution based on condition key.
+
+    If the key contains ``DS##``, where ``##`` denotes the spatial resolution
+    in micron/voxel, spatial resolution is estimated from ``##``. Otherwise,
+    the default spatial resolution is 1 micron/voxel.
+
+    Parameters
+    ----------
+    key
+        Condition key.
+
+    Returns
+    -------
+    :
+        Spatial resolution (micron/voxel).
+    """
+
     match = re.findall(r"[_]?DS([0-9]+)[_]?", key)
 
     if len(match) == 1:
