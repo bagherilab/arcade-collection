8952c9910e8524e9853588e698a02ab119bff1d2
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-09-04 11:44:55 -0400

    Add docstrings and unit tests for extract tick json task

--- src/arcade_collection/output/extract_tick_json.py
+++ src/arcade_collection/output/extract_tick_json.py
@@ -12,6 +12,47 @@ def extract_tick_json(
     extension: Optional[str] = None,
     field: Optional[str] = None,
 ) -> list:
+    """
+    Extract json for specified tick from tar archive.
+
+    For v3 simulations, each tick is saved as a separate json file in the
+    archive. The file names are formatted as ``<key>_<tick>.json`` or
+    ``<key>_<tick>.<extension>.json`` where tick is padded to six digits. Use an
+    integer tick to extract the desired tick from the archive.
+
+    For v2 simulations, all ticks are saved to the same file in the archive. The
+    file name is given as ``<key>.json`` or ``<key>.<extension>.json``. Use a
+    float tick and field to extract the desired tick from the archive.
+
+    .. code-block:: python
+
+        {
+            "timepoints": [
+                {
+                    "time": <time>, "field": <field>, ...
+                }
+            ]
+        }
+
+    Parameters
+    ----------
+    tar
+        Tar archive.
+    key
+        Simulation key.
+    tick
+        Tick to extract.
+    extension
+        Additional extension in file name.
+    field
+        Field in json to extract (only used with float ticks).
+
+    Returns
+    -------
+    :
+        Archive contents for specified tick.
+    """
+
     formatted_tick = f"_{tick:06d}" if isinstance(tick, (int, np.integer)) else ""
 
     if extension is None:
