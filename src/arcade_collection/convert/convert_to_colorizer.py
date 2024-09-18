f123126f39c696f5d3d556d13304e7023ecf4463
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-09-18 15:25:28 -0400

    Add docstrings and unit tests for convert to colorizer task

--- src/arcade_collection/convert/convert_to_colorizer.py
+++ src/arcade_collection/convert/convert_to_colorizer.py
@@ -1,77 +1,145 @@
-import numpy as np
-import pandas as pd
+from __future__ import annotations
 
-from arcade_collection.output.convert_model_units import convert_model_units
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+if TYPE_CHECKING:
+    import pandas as pd
 
 
 def convert_to_colorizer(
-    all_data: pd.DataFrame,
-    features: list[str],
-    frame_spec: tuple[int, int, int],
-    ds: float,
-    dt: float,
-    regions: list[str],
+    all_data: pd.DataFrame, features: list[tuple[str, str, str]], frame_spec: tuple[int, int, int]
 ) -> dict:
+    """
+    Generate TFE manifest and feature data for simulation.
+
+    Parameters
+    ----------
+    all_data
+        Simulation data containing ID, TICK, and time.
+    features
+        List of feature keys, names, and data types.
+    frame_spec
+        Specification for frames.
+
+    Returns
+    -------
+    :
+        TFE manifest and feature data
+    """
+
     frames = list(np.arange(*frame_spec))
     manifest = get_manifest_data(features, frames)
 
-    convert_model_units(all_data, ds, dt, regions)
     frame_data = all_data[all_data["TICK"].isin(frames)]
 
-    outliers = get_outliers_from_data(frame_data)
     tracks = get_tracks_from_data(frame_data)
     times = get_times_from_data(frame_data)
 
-    colorizer_json = {
-        "manifest": manifest,
-        "outliers": outliers,
-        "tracks": tracks,
-        "times": times,
-    }
+    colorizer_json = {"manifest": manifest, "tracks": tracks, "times": times, "features": {}}
 
-    for feature in features:
-        colorizer_json[feature] = get_feature_from_data(frame_data, feature)
+    for index, (key, _, dtype) in enumerate(features):
+        if dtype == "categorical":
+            categories = list(all_data[key].unique())
+            manifest["features"][index]["categories"] = categories
+        else:
+            categories = None
+
+        colorizer_json["features"][key] = get_feature_from_data(frame_data, key, categories)
 
     return colorizer_json
 
 
-def get_manifest_data(features: list[str], frames: list[int]) -> dict:
-    manifest = {
-        "frames": [f"frame_{i}.png" for i in range(len(frames))],
-        "features": {
-            feature: f"feature_{feature_index}.json"
-            for feature_index, feature in enumerate(features)
-        },
-        "outliers": "outliers.json",
+def get_manifest_data(features: list[tuple[str, str, str]], frames: list[int]) -> dict:
+    """
+    Build manifest for TFE.
+
+    Parameters
+    ----------
+    features
+        List of feature keys, names, and data types.
+    frames
+        List of frames.
+
+    Returns
+    -------
+    :
+        Manifest in TFE format.
+    """
+
+    return {
+        "frames": [f"frames/frame_{i}.png" for i in range(len(frames))],
+        "features": [
+            {"key": key, "name": name, "data": f"features/{key}.json", "type": dtype}
+            for key, name, dtype in features
+        ],
         "tracks": "tracks.json",
         "times": "times.json",
     }
 
-    return manifest
-
-
-def get_outliers_from_data(data: pd.DataFrame) -> dict:
-    outliers = [False] * len(data)
-    outliers_json = {"data": outliers, "min": False, "max": True}
-    return outliers_json
-
 
 def get_tracks_from_data(data: pd.DataFrame) -> dict:
-    tracks = data["ID"]
-    tracks_json = {"data": list(tracks)}
-    return tracks_json
+    """
+    Extract track ids from data and format for TFE.
+
+    Parameters
+    ----------
+    data
+        Simulation data for selected frames.
+
+    Returns
+    -------
+    :
+        Track data in TFE format.
+    """
+
+    return {"data": [0, *list(data["ID"])]}
 
 
 def get_times_from_data(data: pd.DataFrame) -> dict:
-    times = data["time"]
-    times_json = {"data": list(times)}
-    return times_json
+    """
+    Extract time points from data and format for TFE.
+
+    Parameters
+    ----------
+    data
+        Simulation data for selected frames.
+
+    Returns
+    -------
+    :
+        Time data in TFE format.
+    """
+
+    return {"data": [0, *list(data["time"])]}
 
 
-def get_feature_from_data(data: pd.DataFrame, feature: str) -> dict:
-    feature_values = data[feature]
+def get_feature_from_data(data: pd.DataFrame, feature: str, categories: list | None = None) -> dict:
+    """
+    Extract specified feature from data and format for TFE.
+
+    Parameters
+    ----------
+    data
+        Simulation data for selected frames.
+    feature
+        Feature key.
+    categories
+        List of data categories (if data is categorical).
+
+    Returns
+    -------
+    :
+        Feature data in TFE format.
+    """
+
+    if categories is not None:
+        feature_values = data[feature].apply(lambda x: categories.index(x))
+    else:
+        feature_values = data[feature]
+
     feature_min = float(np.nanmin(feature_values))
     feature_max = float(np.nanmax(feature_values))
 
-    feature_json = {"data": list(feature_values), "min": feature_min, "max": feature_max}
-    return feature_json
+    return {"data": [0, *list(feature_values)], "min": feature_min, "max": feature_max}
---
+++ tests/arcade_collection/convert/test_convert_to_colorizer.py
@@ -0,0 +1,72 @@
+import unittest
+
+import pandas as pd
+
+from arcade_collection.convert.convert_to_colorizer import convert_to_colorizer
+
+
+class TestConvertToColorizer(unittest.TestCase):
+    def test_convert_to_colorizer(self):
+        all_data = pd.DataFrame(
+            {
+                "TICK": [0, 0, 0, 0, 5, 5, 5, 10, 10, 10, 15, 15],
+                "ID": [1, 2, 4, 5, 1, 4, 5, 1, 2, 4, 1, 2],
+                "time": [0, 0, 0, 0, 0.5, 0.5, 0.5, 1, 1, 1, 1.5, 1.5],
+                "feature_a": [10, 12, 14, 16, 10, 12, 15, 11, 12, 15, 16, 10],
+                "feature_b": ["A", "B", "A", "B", "A", "B", "C", "C", "C", "A", "B", "C"],
+                "feature_c": [1.0, 1.2, 1.4, 1.6, 1.0, 1.2, 1.5, 1.1, 1.2, 1.5, 1.6, 1.0],
+            }
+        )
+        features = [
+            ("feature_a", "Feature A name", "continuous"),
+            ("feature_b", "Feature B name", "categorical"),
+            ("feature_c", "Feature C name", "discrete"),
+        ]
+        frame_spec = (0, 16, 10)
+
+        expected_manifest = {
+            "frames": ["frames/frame_0.png", "frames/frame_1.png"],
+            "features": [
+                {
+                    "key": "feature_a",
+                    "name": "Feature A name",
+                    "data": "features/feature_a.json",
+                    "type": "continuous",
+                },
+                {
+                    "key": "feature_b",
+                    "name": "Feature B name",
+                    "data": "features/feature_b.json",
+                    "type": "categorical",
+                    "categories": ["A", "B", "C"],
+                },
+                {
+                    "key": "feature_c",
+                    "name": "Feature C name",
+                    "data": "features/feature_c.json",
+                    "type": "discrete",
+                },
+            ],
+            "tracks": "tracks.json",
+            "times": "times.json",
+        }
+
+        expected_tracks = {"data": [0, 1, 2, 4, 5, 1, 2, 4]}
+        expected_times = {"data": [0, 0, 0, 0, 0, 1, 1, 1]}
+
+        expected_feature_a = {"data": [0, 10, 12, 14, 16, 11, 12, 15], "min": 10, "max": 16}
+        expected_feature_b = {"data": [0, 0, 1, 0, 1, 2, 2, 0], "min": 0, "max": 2}
+        expected_feature_c = {"data": [0, 1, 1.2, 1.4, 1.6, 1.1, 1.2, 1.5], "min": 1, "max": 1.6}
+
+        tfe = convert_to_colorizer(all_data, features, frame_spec)
+
+        self.assertDictEqual(expected_manifest, tfe["manifest"])
+        self.assertDictEqual(expected_tracks, tfe["tracks"])
+        self.assertDictEqual(expected_times, tfe["times"])
+        self.assertDictEqual(expected_feature_a, tfe["features"]["feature_a"])
+        self.assertDictEqual(expected_feature_b, tfe["features"]["feature_b"])
+        self.assertDictEqual(expected_feature_c, tfe["features"]["feature_c"])
+
+
+if __name__ == "__main__":
+    unittest.main()
